389d66379b4b59996ea108556feddeed89a2f74e
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2023-02-27 18:30:38 +0700

    added folder

---
+++ lib/data/models/activity_model.dart
@@ -0,0 +1,40 @@
+class ActivityModel {
+  String? activity;
+  String? type;
+  int? participants;
+  int? price;
+  String? link;
+  String? key;
+  double? accessibility;
+
+  ActivityModel(
+      {this.activity,
+      this.type,
+      this.participants,
+      this.price,
+      this.link,
+      this.key,
+      this.accessibility});
+
+  ActivityModel.fromJson(Map<String, dynamic> json) {
+    activity = json['activity'];
+    type = json['type'];
+    participants = json['participants'];
+    price = json['price'];
+    link = json['link'];
+    key = json['key'];
+    accessibility = json['accessibility'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['activity'] = this.activity;
+    data['type'] = this.type;
+    data['participants'] = this.participants;
+    data['price'] = this.price;
+    data['link'] = this.link;
+    data['key'] = this.key;
+    data['accessibility'] = this.accessibility;
+    return data;
+  }
+}
